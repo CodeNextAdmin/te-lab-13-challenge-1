fe766ee84f4a7e15642120792cf453f29eba75a8
Glitch (hello-webpage) <none>
2020-05-22 17:34:57 +0000

    📙🕶 Checkpoint ./.glitch-assets:15663604/2273 ./script.js:15663604/6486

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-/* this is your playground */
+/*************************** Challenge 1 ************************/
 
-let jokesArray =[
-  
- "Q. Why aren’t Koalas actual bears? A. They don’t meet the koalifications.",
-  "Q. What happens to a frog’s car when it breaks down? A. It gets toad away.", 
+let jokesArray = [
+  "Q. Why aren’t Koalas actual bears? A. They don’t meet the koalifications.",
+  "Q. What happens to a frog’s car when it breaks down? A. It gets toad away.",
   "Q. What do you call bears with no ears? A. B",
-  "Q: What do you call an old snowman? \n A: A creek.", 
+  "Q: What do you call an old snowman? \n A: A creek.",
   "Q: Why can’t you trust an atom? A: Because they make up everything.",
-  "Q: What did the duck say when he bought lipstick? A: “Put it on my bill.", 
+  "Q: What did the duck say when he bought lipstick? A: “Put it on my bill.",
   "Q: Did you hear about the kidnapping at school? A: It’s okay. He woke up.",
-  "Q. How did the hipster burn his mouth? A. He ate the pizza before it was cool.", 
+  "Q. How did the hipster burn his mouth? A. He ate the pizza before it was cool.",
   "Q: What do you call a pig that does karate? A: A pork chop.",
-  "Q: Why couldn’t Cinderella play soccer? A: She kept running away from the ball.", 
+  "Q: Why couldn’t Cinderella play soccer? A: She kept running away from the ball.",
   "Q: What do you call a boomerang that won’t come back? A: A stick.",
-  "Q. What’s the difference between ignorance and apathy? A. I don’t know, and I don’t care.", 
+  "Q. What’s the difference between ignorance and apathy? A. I don’t know, and I don’t care.",
   "Q. What did one DNA strand say to the other? A. Does my ass look big in these genes?",
-  "I thought my neighbours were lovely people. Then they went and put a password on their wi-fi.", 
+  "I thought my neighbours were lovely people. Then they went and put a password on their wi-fi.",
   "I've just opened a new restaurant called Karma. There's no menu, we just give you what you deserve.",
-  "Q. Why couldn't the teacher control her pupils? A. She couldn't find her glasses.", 
+  "Q. Why couldn't the teacher control her pupils? A. She couldn't find her glasses.",
   "Q. What book won't teachers give you credit for reading? A. Facebook."
-]
+];
 
-function randomJoke(){
-  
+function randomJoke() {
   /*Challenge 2: display a random joke when the user clicks on the card.
   /* traverse the array! use the random functions */
-  
-  let joke = ""
-  
+
+  let joke = "";
+
   /* alert("random!") */
-  
-  return joke
+
+  return joke;
 }
+
+/*************************** Challenge 2 ************************/
+
+let pics = [
+  "https://cdn.glitch.com/6f1555ee-2e0c-464b-8c54-9d1c5c073672%2Fclouds1.jpg?v=1590168390533",
+  "https://cdn.glitch.com/6f1555ee-2e0c-464b-8c54-9d1c5c073672%2Fclouds2.jpg?v=1590168407025",
+  "https://cdn.glitch.com/6f1555ee-2e0c-464b-8c54-9d1c5c073672%2Fclouds3.jpg?v=1590168421906",
+  "https://cdn.glitch.com/6f1555ee-2e0c-464b-8c54-9d1c5c073672%2Fclouds4.jpg?v=1590168436521",
+  "https://cdn.glitch.com/6f1555ee-2e0c-464b-8c54-9d1c5c073672%2Fclouds5.jpg?v=1590168450002",
+  "https://cdn.glitch.com/6f1555ee-2e0c-464b-8c54-9d1c5c073672%2Fclouds6.jpg?v=1590168462649"
+];
+
+/*write a function that sets the NEXT pic, when it reaches the end, it loads the 0 index again. */
