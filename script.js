cff51baa7f0cafe07718e2ab5425e35195b5d766
Glitch (hello-webpage) <none>
2020-05-22 05:13:47 +0000

    🚠🌗 Checkpoint ./script.js:15663604/1752

--- script.js
+++ script.js
@@ -2,8 +2,26 @@
 
 let jokesArray =[
   
-   Why aren’t Koalas actual bears? They don’t meet the koalifications.
-  
-  
-  
+ "Q. Why aren’t Koalas actual bears? A. They don’t meet the koalifications.",
+  "Q. What happens to a frog’s car when it breaks down? A. It gets toad away.", 
+  "Q. What do you call bears with no ears? A. B",
+  "Q: What do you call an old snowman? \n A: A creek.", 
+  "Q: Why can’t you trust an atom? A: Because they make up everything.",
+  "Q: What did the duck say when he bought lipstick? A: “Put it on my bill.", 
+  "Q: Did you hear about the kidnapping at school? A: It’s okay. He woke up.",
+  "Q. How did the hipster burn his mouth? A. He ate the pizza before it was cool.", 
+  "Q: What do you call a pig that does karate? A: A pork chop.",
+  "Q: Why couldn’t Cinderella play soccer? A: She kept running away from the ball.", 
+  "Q: What do you call a boomerang that won’t come back? A: A stick.",
+  "Q. What’s the difference between ignorance and apathy? A. I don’t know, and I don’t care.", 
+  "Q. What did one DNA strand say to the other? A. Does my ass look big in these genes?",
+  "I thought my neighbours were lovely people. Then they went and put a password on their wi-fi.", 
+  "I've just opened a new restaurant called Karma. There's no menu, we just give you what you deserve.",
+  "Q. Why couldn't the teacher control her pupils? A. She couldn't find her glasses.", 
+  "Q. What book won't teachers give you credit for reading? A. Facebook."
 ]
+
+function randomJoke(){
+  
+  return joke
+}
